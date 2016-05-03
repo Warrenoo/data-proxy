eead1d0a2697f22a7ffac7ad271596c011e179bb
沈汪洋 <541991a@gmail.com>
2016-05-03 11:59:25 +0800

    panic redis connection error

--- libs/redis.go
+++ libs/redis.go
@@ -6,6 +6,9 @@ import (
 )
 
 func InitRedis(redis_host string, redis_port string) redis.Conn {
-	conn, _ := redis.DialTimeout("tcp", redis_host+":"+redis_port, 0, 1*time.Second, 1*time.Second)
+	conn, err := redis.DialTimeout("tcp", redis_host+":"+redis_port, 0, 1*time.Second, 1*time.Second)
+	if err != nil {
+		panic(err.Error())
+	}
 	return conn
 }
